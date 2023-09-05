9fb7449c874f3f6c08da6d98a4752c7107d0c42a
unknown <info@mostbest.ru>
2023-09-05 15:20:51 +0300

    callback

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,24 +1,55 @@
-const l = (e) => console.log(e);
+const log = (e) => console.log(e);
 
 function getDomElementByClassName(name) {
     return document.querySelector(name);
 }
 
+function getElem(name) {
+    
+}
 
 function callbackAnimate(time) {
     let elem = getDomElementByClassName('.callback');
 
     setInterval(() => {
-        elem.classList.add('drop');
+        elem.classList.add('animate-drop');
+
         setTimeout(() => {
-            elem.classList.remove('drop');
+            elem.classList.remove('animate-drop');
         }, 1000);
     }, time);
 }
 
-setTimeout(() => {
-    let elem = getDomElementByClassName('.callback');
-    elem.classList.add('show');
-}, 1000);
+function hideCallback(name) {
+    let node = getDomElementByClassName(name);
+    let btn = getDomElementByClassName('.callback__btn-close'); 
 
-callbackAnimate(15000);
+    node.addEventListener('click', () => {
+        log();
+    });
+
+    // node.classList.add('animate-hide');
+    // node.classList.remove('callback_show');
+}
+
+
+
+
+function showCallback(params) {
+    let node = getDomElementByClassName(params.nodeClassName);
+    setTimeout(() => {
+        node.classList.add('animate-show');
+        node.addEventListener('animationend', () => {
+            node.classList.add('callback_show');
+            node.classList.remove('animate-show');
+            if (params.addAnimationDrop) callbackAnimate(params.timeAnimateDrop);
+        });
+    }, params.timeAnimateShow);
+}
+
+showCallback({
+    nodeClassName: '.callback',
+    timeAnimateShow: 2000,
+    addAnimationDrop: false,
+    timeAnimateDrop: 6000
+});
